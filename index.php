8f635816827116b4c4df490aa1d47ca6a939fd2f
pascalid <32020043+pascalid@users.noreply.github.com>
2017-11-25 08:24:33 -0500

    fichiers
    ajout des fichiers pour le TP3

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>formulaire uqam</title>
+	<script src="formulaire.js"></script>
+</head>
+<body>
+	<h1>Formulaire d'inscription à l'uqam</h1>
+</body>
---
+++ scripts/formulaire.js
@@ -0,0 +1,46 @@
+function validerForm(){
+	effacerErr();
+	var nom = champObligatoire("nom", "err_nom");
+	var prenom = champObligatoire("prenom", "err_pre");
+	var date = champObligatoire("date", "err_date") && validerDate("err_date");
+	var code = champObligatoire("code", "err_code");
+	var genre = champObligatoire("genre", "err_genre") && validerGenre("err_genre");
+	return nom && prenom && date && code && genre;
+}
+
+function effacerErr(){
+	var errs = document.getElementsByClassName("err");
+	var e;
+	for(e=0; e<errs.length; e++){
+		errs[e].innerHTML="";
+	}
+}
+
+function champObligatoire(inputId, spanId){
+	var valeur = document.getElementById(inputId).value;
+	
+	if(valeur == null || valeur == ''){
+		document.getElementById(spanId).innerHTML = "Ce champ doit contenir une valeur";
+		return false;
+	}
+	return true;
+}
+
+function validerDate(spanId){
+	var valeur = document.getElementById("date").value;
+	var regex = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+	if(!regex.test(valeur)){
+		document.getElementById(spanId).innerHTML = "date invalide!";
+		return false;
+	}
+	return true;
+}
+
+function validerGenre(spanId){
+	var valeur = document.getElementById("genre").value;
+	if(valeur != "F" && valeur != "M"){
+		document.getElementById(spanId).innerHTML = "F ou M seulement";
+		return false;
+	}
+	return true;
+}
